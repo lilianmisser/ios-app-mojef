40fb2f511d27ca0dc1917f6d771f1d72cc2c451f
Lilian MISSER <lilian.misser@gmail.com>
2021-03-24 16:15:14 +0100

    initial commit

---
+++ ios-app-mojef/ios_app_mojefApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_mojefApp.swift
+//  ios-app-mojef
+//
+//  Created by etud on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_mojefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-app-mojefTests/ios_app_mojefTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_app_mojefTests.swift
+//  ios-app-mojefTests
+//
+//  Created by etud on 24/03/2021.
+//
+
+import XCTest
+@testable import ios_app_mojef
+
+class ios_app_mojefTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
